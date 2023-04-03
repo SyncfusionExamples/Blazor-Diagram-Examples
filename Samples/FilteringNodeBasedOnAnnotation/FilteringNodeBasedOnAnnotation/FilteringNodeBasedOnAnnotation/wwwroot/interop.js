f400593870cc64f9bc5eeb90263288a85af4b05b
AzureAD\BalavigneshRaviChand <balavignesh.ravichandran@syncfusion.com>
2023-04-03 22:32:23 +0530

    Story(BLAZ-30397):Filtering node based on annotation sample

---
+++ Samples/FilteringNodeBasedOnAnnotation/FilteringNodeBasedOnAnnotation/FilteringNodeBasedOnAnnotation/wwwroot/interop.js
@@ -0,0 +1,8 @@
+﻿window.getDiagramViewportBounds = function () {
+
+    var bounds = document.getElementsByClassName('e-control e-diagram e-lib e-droppable e-tooltip');
+
+    return bounds[0].getBoundingClientRect();
+
+}
+

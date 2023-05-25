e4afcac4c6fbd1ccc2144f10e0123e17f90d2e28
AzureAD\BalavigneshRaviChand <balavignesh.ravichandran@syncfusion.com>
2023-05-25 17:16:30 +0530

    Task(ES-830622):Example sample for exporting diagram to pdf using html markup

---
+++ KB-Samples/HTMLToPdfExport/wwwroot/scripts.js
@@ -0,0 +1,157 @@
+﻿
+window.getDiagramContent = function getDiagramContent(id) {
+
+    var diagram = document.getElementById(id).innerHTML;
+    return diagram;
+}
+
+window.getDiagramBounds = function getDiagramBounds(id) {
+    var diagram = document.getElementById(id);
+    var bounds = diagram.getBoundingClientRect();
+    return bounds;
+}
+
+window.download = function download(image) {
+    var el = document.createElement("a");
+    el.setAttribute("href", image);
+    el.setAttribute("download", "diagram");
+    el.click();
+    el.remove();
+}
+window.saveAsFile = function saveAsFile(filename, bytesBase64) {
+    if (navigator.msSaveBlob) {
+        //Download document in Edge browser
+        var data = window.atob(bytesBase64);
+        var bytes = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            bytes[i] = data.charCodeAt(i);
+        }
+        var blob = new Blob([bytes.buffer], { type: "application/octet-stream" });
+        navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        var link = document.createElement('a');
+        link.download = filename;
+        link.href = "data:application/octet-stream;base64," + bytesBase64;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+    }
+}
+function CommonKeyboardCommands_newDiagram() {
+    var origin = window.location.origin;
+    if (!origin) {
+        origin = window.location.protocol + '//'
+            + window.location.hostname
+            + (window.location.port ? ':' + window.location.port : '');
+    }
+    window.open(origin + window.location.pathname);
+};
+function getDiagramFileName(dialogName) {
+    if (dialogName === 'export')
+        return document.getElementById('diagramName').innerHTML.toString();
+    if (dialogName === 'save')
+        return document.getElementById('diagramName').value.toString();
+    else
+        return document.getElementById('diagramName').innerHTML.toString();
+}
+function saveDiagram(data, filename) {
+    if (window.navigator.msSaveBlob) {
+        let blob = new Blob([data], { type: 'data:text/json;charset=utf-8,' });
+        window.navigator.msSaveOrOpenBlob(blob, filename + '.json');
+    } else {
+        let dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(data);
+        let a = document.createElement('a');
+        a.href = dataStr;
+        a.download = filename + '.json';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    }
+}
+UtilityMethods_hideElements = function (elementType, diagramType) {
+    var diagramContainer = document.getElementsByClassName('diagrambuilder-container')[0];
+    if (diagramContainer.classList.contains(elementType)) {
+        if (!(diagramType === 'mindmap-diagram' || diagramType === 'orgchart-diagram')) {
+            diagramContainer.classList.remove(elementType);
+        }
+    }
+    else {
+        diagramContainer.classList.add(elementType);
+    }
+};
+function hideMenubar() {
+    UtilityMethods_hideElements('hide-menubar');
+}
+function getHyperLinkValueFromDocument(id, attribute) {
+    return document.getElementById(id).value;
+}
+function click() {
+    document.getElementById('UploadFiles').click();
+}
+function hideElements(elementType) {
+    var diagramContainer = document.getElementsByClassName('diagrambuilder-container')[0];
+    if (diagramContainer.classList.contains(elementType)) {
+        diagramContainer.classList.remove(elementType);
+    } else {
+        diagramContainer.classList.add(elementType);
+    }
+}
+//function click() {
+//    document.getElementById('defaultfileupload').click();
+//}
+function loadFile(file) {
+    var base64 = file.rawFile.replace("data:application/json;base64,", "");
+    var json = atob(base64)
+    return json;
+}
+function loadDiagram(event) {
+    return event.target.result.toString();
+}
+window.downloadPdf = function downloadPdf(base64String, fileName) {
+    var sliceSize = 512;
+    var byteCharacters = atob(base64String);
+    var byteArrays = [];
+
+
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+        var byteNumbers = new Array(slice.length);
+
+
+
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+
+
+        var byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+
+
+
+    var blob = new Blob(byteArrays, {
+        type: 'application/pdf'
+    });
+    var blobUrl = window.URL.createObjectURL(blob);
+    this.triggerDownload("PDF", fileName, blobUrl);
+}
+window.onChangeScrollValues = function (element, left, top, eventValue) {
+    if (element && left && top) {
+        if (window.eventInvokeValue == eventValue) {
+            window.eventStarted = false;
+        }
+        element.scrollLeft = left;
+        element.scrollTop = top;
+    }
+}
+triggerDownload: function triggerDownload(type, fileName, url) {
+    var anchorElement = document.createElement('a');
+    anchorElement.download = fileName + '.' + type.toLocaleLowerCase();
+    anchorElement.href = url;
+    anchorElement.click();
+}
+

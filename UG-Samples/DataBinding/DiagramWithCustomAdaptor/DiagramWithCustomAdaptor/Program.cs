e661caf39f818ac0141cde450c3bafca916eed8a
BalaVigneshRaviChandran <balavignesh.ravichandran@syncfusion.com>
2024-09-19 15:11:31 +0530

    892709: CRUD support in diagram control

--- UG-Samples/DataBinding/DiagramWithCustomAdaptor/DiagramWithCustomAdaptor/Program.cs
+++ UG-Samples/DataBinding/DiagramWithCustomAdaptor/DiagramWithCustomAdaptor/Program.cs
@@ -1,10 +1,13 @@
-using DiagramWithCustomAdaptor.Components;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Syncfusion.Blazor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
@@ -12,7 +15,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,9 +23,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();

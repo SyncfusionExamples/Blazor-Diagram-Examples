dd42b5e00b2370cf0665099a95812a72aa14c436
BalaVigneshRaviChandran <balavignesh.ravichandran@syncfusion.com>
2024-11-06 10:03:28 +0530

    917438: KB and samples review

---
+++ KB-Samples/DiagramWithListBox/README.md
@@ -0,0 +1,17 @@
+#Hierarchical Layout Sample
+
+This sample shows how to arrange the nodes in an hierarchical structure. The hierarchical tree layout arranges nodes in a tree-like structure, where the nodes in the hierarchical layout may have multiple parents. There is no need to specify the layout root. To arrange the nodes in an hierarchical structure, specify the layout Type as HierarchicalTree.
+
+Demo link:
+https://blazor.syncfusion.com/demos/diagramcomponent/hierarchical-tree?theme=fluent
+
+## Prerequisites
+
+* Visual Studio 2022
+
+## How to run the project
+
+* Checkout this project to a location in your disk.
+* Open the solution file using the Visual Studio 2022.
+* Restore the NuGet packages by rebuilding the solution.
+* Run the project.
---
+++ KB-Samples/DiagramWithListBox/wwwroot/JavaScript.js
@@ -0,0 +1,78 @@
+﻿window.InvokeClockEvent1 = (dotNetHelper, diagramID) => {
+    var diagram = document.getElementById(diagramID);
+    var wheelHandler = function (e) {
+        var bounds = measureScrollValues(e.currentTarget.id);
+        e.diagramCanvasScrollBounds = bounds;
+        e.diagramGetBoundingClientRect = e.currentTarget.getBoundingClientRect();
+        //var zoom = e.wheelDelta > 0 ? true : false;
+        ///*  prevent the default behavior of the event*/
+        //e.preventDefault();
+        ///* Stop the event from propagating further*/
+        //e.stopPropagation();
+        var sss = window.scrollX.toString() + "adsh" + window.scrollY.toString();
+        dotNetHelper.invokeMethodAsync('ChangeState', sss);
+    };
+    diagram.addEventListener('mouseup', wheelHandler, true);
+};
+ function getlistViewWidth(id){
+
+     var idw = document.getElementById(id);
+     return idw.offsetWidth.toString();
+}
+measureScrollValues: function measureScrollValues(diagramId) {
+    var element = document.getElementById(diagramId);
+    var point = new Rect(element.scrollLeft, element.scrollTop, element.scrollWidth, element.scrollHeight);
+    return point;
+}
+function InvokeClockEvent(dotNetHelper, diagramID) {
+    var sss = window.scrollX.toString() + "adsh" + window.scrollY.toString();
+    return sss;
+}
+
+var Rect = /** @class */function () {
+    function Rect(x, y, width, height) {
+        /**
+         * Sets the x-coordinate of the starting point of a rectangular region
+         *
+         * @default 0
+         */
+        this.x = Number.MAX_VALUE;
+        /**
+         * Sets the y-coordinate of the starting point of a rectangular region
+         *
+         * @default 0
+         */
+        this.y = Number.MAX_VALUE;
+        /**
+         * Sets the width of a rectangular region
+         *
+         * @default 0
+         */
+        this.width = 0;
+        /**
+         * Sets the height of a rectangular region
+         *
+         * @default 0
+         */
+        this.height = 0;
+        if (x === undefined || y === undefined) {
+            x = y = Number.MAX_VALUE;
+            width = height = 0;
+        } else {
+            if (width === undefined) {
+                width = 0;
+            }
+            if (height === undefined) {
+                height = 0;
+            }
+        }
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    /**   @private  */
+    Rect.empty = new Rect(Number.MAX_VALUE, Number.MIN_VALUE, 0, 0);
+    return Rect;
+}();
+var args = {};

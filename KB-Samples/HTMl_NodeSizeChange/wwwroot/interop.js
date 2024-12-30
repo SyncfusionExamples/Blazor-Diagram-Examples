b2190cc99ebbbf43de440656488d6f2a6e180c62
BalaVigneshRaviChandran <balavignesh.ravichandran@syncfusion.com>
2024-12-30 21:34:50 +0530

    906256: KB samples

---
+++ KB-Samples/HTMl_NodeSizeChange/wwwroot/interop.js
@@ -0,0 +1,11 @@
+﻿window.textAreaResize = (elementId) => {
+    const textarea = document.getElementById(elementId);
+    const height = textarea.scrollHeight;
+    const width = textarea.scrollWidth;
+  const container = textarea.parentElement;
+    
+    textarea.style.height = 'auto'; // Reset height to auto
+    textarea.style.height = Math.min(container.clientHeight, textarea.scrollHeight) + 'px'; // Set height to content size or container max height
+
+    return { height, width };
+}

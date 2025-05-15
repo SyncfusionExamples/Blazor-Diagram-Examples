25010846ccddd59e56617276e0a4fff15488cf62
Jawin-SF4519 <165898284+Jawin-SF4519@users.noreply.github.com>
2025-05-15 10:30:34 +0530

    958035: PageBreak sample

---
+++ KB-Samples/BeyondPageBreak/BeyondPageBreak/wwwroot/Interop.js
@@ -0,0 +1,13 @@
+﻿function getViewportBounds() {
+    var parentElement = document.getElementsByClassName('e-background-layer')[0];
+    var firstChild = parentElement.firstElementChild; // Get the first child element
+
+    // Get x, y attributes (they are stored as strings)
+    var x = parseFloat(firstChild.getAttribute('x')) || 0;
+    var y = parseFloat(firstChild.getAttribute('y')) || 0;
+    var width = parseFloat(firstChild.getAttribute('width')) || 0;
+    var height = parseFloat(firstChild.getAttribute('height')) || 0;
+
+
+    return { x, y, width, height };
+}

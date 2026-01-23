daa1ddf531830fb94235718855156d53a3544e0f
Ramya <ramya.subramani@syncfusion.com>
2026-01-23 18:28:06 +0530

    1002377: Added AI samples for UG

---
+++ UG-Samples/AISamples/MauiAppAISample/FlowchartSample/FlowchartSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlowchartSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ UG-Samples/AISamples/ServerAISample/MindmapSample/MindmapSample/Program.cs
@@ -0,0 +1,48 @@
+using AISample.Components;
+using AIService;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
+using System.ClientModel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+
+string apiKey = "AZURE_OPENAI_KEY";
+string deploymentName = "AZURE_OPENAI_MODEL";
+string endpoint = "AZURE_OPENAI_ENDPOINT";
+
+AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(
+     new Uri(endpoint),
+     new ApiKeyCredential(apiKey)
+);
+
+IChatClient AIChatClient = azureOpenAIClient.GetChatClient(deploymentName).AsIChatClient();
+builder.Services.AddScoped<AzureAIService>(serviceProvider =>
+{
+    return new AzureAIService(AIChatClient);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();

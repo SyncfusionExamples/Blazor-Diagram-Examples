de5f85ec2beeda326d176053a262f5ff9827d0ea
Ramya <ramya.subramani@syncfusion.com>
2026-08-17 10:38:59 +0530

    1042047: Created Unselect Symbol Sample

---
+++ KB-Samples/UnselectSymbolInPalette/UnselectSymbolInPalette/wwwroot/Javascript.js
@@ -0,0 +1,10 @@
+﻿// Function to remove background from the selected symbol
+function removeSelectedSymbolBackground() {
+    // Query the selected element
+    var selectedElement = document.querySelector('.e-symbolpalette .e-symbol-selected');
+
+    if (selectedElement) {
+        selectedElement.classList.remove("e-symbol-selected");
+        selectedElement.classList.remove("e-symbol-hover");
+    }
+}

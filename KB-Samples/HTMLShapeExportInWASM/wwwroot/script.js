fda4a8c45fe63819c22e652ba11be0093129a807
Ramya <ramya.subramani@syncfusion.com>
2026-08-17 12:46:27 +0530

    1042053: Updated the sample

--- KB-Samples/HTMLShapeExportInWASM/wwwroot/script.js
+++ KB-Samples/HTMLShapeExportInWASM/wwwroot/script.js
@@ -5,11 +5,10 @@ window.getDiagramContent = function getDiagramContent(id) {
 }
 
 window.getDiagramBounds = function getDiagramBounds(id) {
-    var diagram = document.getElementById(id);
+    var diagram = document.getElementById(id + "_diagramLayer_div");
     var bounds = diagram.getBoundingClientRect();
     return bounds;
 }
-
 window.download = function download(image) {
     var el = document.createElement("a");
     el.setAttribute("href", image);
@@ -18,23 +17,43 @@ window.download = function download(image) {
     el.remove();
 }
 
-window.exportToImage = async function(elementId)
-{
-    var element = document.querySelector("#" + elementId);
+window.exportToImage = async function (elementId) {
     var image = "";
-    await html2canvas(element).then(canvas => image = canvas.toDataURL("image/png"));
+    const original1 = document.getElementById(elementId + '_diagramLayer_div');
+    const original2 = document.getElementById(elementId + '_htmlLayer');
+    const clone1 = original1.cloneNode(true);
+    const clone2 = original2.cloneNode(true);
+    clone1.appendChild(clone2);
+    const tempContainer = document.createElement('div');
+    tempContainer.style.position = 'absolute';
+    tempContainer.style.left = '-9999px';
+    tempContainer.style.top = '-9999px';
+    tempContainer.appendChild(clone1);
+    document.body.appendChild(tempContainer);
+    await html2canvas(clone1).then(canvas => image = canvas.toDataURL("image/png"));
     var link = document.createElement("a");
     link.href = image;
     link.download = "diagram.png";
     link.click();
 }
 window.exportToPdf = async function (elementId) {
-    var element = document.querySelector("#" + elementId);
+    const original1 = document.getElementById(elementId + '_diagramLayer_div');
+    const original2 = document.getElementById(elementId + '_htmlLayer');
+    const clone1 = original1.cloneNode(true);
+    const clone2 = original2.cloneNode(true);
+    clone1.appendChild(clone2);
+    const tempContainer = document.createElement('div');
+    tempContainer.style.position = 'absolute';
+    tempContainer.style.left = '-9999px';
+    tempContainer.style.top = '-9999px';
+    tempContainer.appendChild(clone1);
+    document.body.appendChild(tempContainer);
     var image = "";
-    await html2canvas(element).then(canvas => image = canvas.toDataURL("image/png"));
-
+    await html2canvas(clone1).then(canvas => image = canvas.toDataURL("image/png"));
+    document.body.removeChild(tempContainer);
     return image;
 }
+
 window.downloadPdf = function (base64String, fileName) {
     var sliceSize = 512;
     var byteCharacters = atob(base64String);
